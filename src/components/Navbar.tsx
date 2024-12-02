8ca2bde9197c6c1389a7d18d03cabaaca6b65f4c
Deepak <96007304+deepakA18@users.noreply.github.com>
2024-12-02 06:32:18 +0000

    final push

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -12,7 +12,7 @@ export default function Navbar() {
       <header className="fixed top-3 border bg-cyan-900/10 backdrop-blur-3xl border-white/10 w-[70%] h-14 pl-3 pr-3 rounded-lg z-50">
         <div className="container flex h-14 items-center">
           <div className="flex items-center gap-2 mr-8">
-            <span className={`font-semibold text-xl`}>KUIPER</span>
+            <span className={`font-semibold text-xl`}>SuiZone</span>
           </div>
           
           <nav className="flex items-center gap-6 text-sm">          
